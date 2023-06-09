5f8b360c90da9fafaeea8ef01b8d18d9ba010c3a
kasumu <kasumuelias@yahoo.com>
2023-06-09 23:07:57 +0100

    model

--- lib/business/model/user.dart
+++ lib/business/model/user.dart
@@ -1,3 +1,15 @@
 class User{
+  final String name;
+  final String email;
+  final int phoneNumber;
+  final bool NewsLetter;
+  final String userBio;
 
+  User({
+    required this.name,
+    required this.email,
+    required this.phoneNumber,
+    required this.NewsLetter,
+    required this.userBio,
+  });
 }
